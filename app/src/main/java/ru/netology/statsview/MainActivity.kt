a4cd4503191b970fc8e828ee529b65d3c58d4de4
[Sergey Melnichenko] <[m1s3@mail.ru]>
2022-12-09 10:47:01 +0600

    Created animation based on the lecture material

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.netology.statsview
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.statsview.ui.StatsView
 
@@ -8,11 +11,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.statsView).data = listOf(
+
+        val view = findViewById<StatsView>(R.id.statsView)
+
+        view.data = listOf(
             500F,
             500F,
             500F,
-            0F,
+            500F,
+        )
+
+        val textView = findViewById<TextView>(R.id.label)
+
+        view.startAnimation(
+            AnimationUtils.loadAnimation(this, R.anim.animation).apply {
+                setAnimationListener(object : Animation.AnimationListener {
+                    override fun onAnimationStart(p0: Animation?) {
+                        textView.text = "onAnimationStart"
+                    }
+
+                    override fun onAnimationEnd(p0: Animation?) {
+                        textView.text = "onAnimationEnd"
+                    }
+
+                    override fun onAnimationRepeat(p0: Animation?) {
+                        textView.text = "onAnimationRepeat"
+                    }
+
+                })
+            }
         )
     }
 }
